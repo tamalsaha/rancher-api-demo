f8b745a74732cbf7751dd8cd2db7bd20304ae124
Tamal Saha <tamal@appscode.com>
2024-09-21 01:21:12 -0700

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,26 @@
 module github.com/tamalsaha/rancher-api-demo
 
 go 1.23.1
+
+require (
+	github.com/rancher/norman v0.0.0-20240708202514-a0127673d1b9
+	github.com/rancher/rancher/pkg/client v0.0.0-20240920230113-fe3ef1af6140
+	sigs.k8s.io/yaml v1.3.0
+)
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rancher/wrangler/v3 v3.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.30.1 // indirect
+	k8s.io/klog/v2 v2.120.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,71 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"sigs.k8s.io/yaml"
+	"time"
 
+	"github.com/rancher/norman/clientbase"
+	"github.com/rancher/norman/types"
+	client "github.com/rancher/rancher/pkg/client/generated/management/v3"
+)
+
+func main() {
+	err := doStuff()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func doStuff() error {
+	opts := clientbase.ClientOpts{
+		URL:        "https://172.233.204.178/v3",
+		TokenKey:   os.Getenv("RANCHER_API_TOKEN"),
+		Timeout:    0,
+		HTTPClient: nil,
+		WSDialer:   nil,
+		CACerts:    "",
+		Insecure:   true,
+		ProxyURL:   "",
+	}
+	rc, err := client.NewClient(&opts)
+	if err != nil {
+		return err
+	}
+	coll, err := rc.Token.ListAll(&types.ListOpts{})
+	if err != nil {
+		return err
+	}
+	for _, item := range coll.Data {
+		fmt.Println(item.Name)
+	}
+
+	token, err := rc.Token.ByID("token-zzknb")
+	if err != nil {
+		return err
+	}
+	// "2024-10-11T03:50:02Z"
+	if token.Expired {
+		return fmt.Errorf("token expired")
+	}
+	expiresAt, err := time.Parse(time.RFC3339, token.ExpiresAt)
+	if err != nil {
+		return err
+	}
+	fmt.Println(token.ExpiresAt, expiresAt.Format(time.RFC3339))
+
+	nt, err := rc.Token.Create(&client.Token{
+		Description: fmt.Sprintf("monitoring-operator-token-%d", time.Now().Unix()),
+		TTLMillis:   90 * 24 * time.Hour.Milliseconds(),
+	})
+	if err != nil {
+		return err
+	}
+	data, err := yaml.Marshal(nt)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(data))
+	return nil
+}
